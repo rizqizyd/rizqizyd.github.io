d8cbeae7e3bd55b7093bc8c5dfa869b6c84d4ce1
Muhammad Rizqi Az Zayyad <muhammadrizqiaz@gmail.com>
2025-01-27 17:08:40 +0700

    updated description

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -13,12 +13,12 @@ function About() {
         <p className="text-justify mb-6 text-[15px]">
           As a Frontend Developer with over three years of experience, I have
           worked on diverse projects, including crowdfunding platforms, fashion
-          e-commerce sites, music streaming applications, and recommendation
-          systems. My expertise lies in developing visually engaging and
-          interactive web applications, collaborating effectively with
-          cross-functional teams, and enhancing functionality and user
-          experience. I specialize in creating responsive, maintainable websites
-          optimized for performance and seamless usability.
+          e-commerce sites, music web apps, and recommendation systems. My
+          expertise lies in developing visually engaging and interactive web
+          applications, collaborating effectively with cross-functional teams,
+          and enhancing functionality and user experience. I specialize in
+          creating responsive, maintainable websites optimized for performance
+          and seamless usability.
           <br />
         </p>
         <Skills />
